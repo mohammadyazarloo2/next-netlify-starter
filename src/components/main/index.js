a129a45572c164c7aa7cd64d3b60a35d587e3a6e
mohammad yazarloo <mohammadyazarloo2@gmail.com>
2021-06-03 02:33:01 +0430

    12 khordad

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -3,23 +3,26 @@ import Image from "next/image";
 import Header from "../../components/layout/Header";
 import styles from "../../styles/Main.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
 
-const Main = ({ children }) => {
-  return (
-    <>
-      <div className={styles.container}>
-        <header className={styles.header}>
-          <Header />
-        </header>
-
-        <div className={styles.main}>
-          {children}
+class Main extends React.Component {
+  render(){
+    return (
+      <>
+        <div className={styles.container}>
+          <header className={styles.header}>
+            <Header />
+          </header>
+  
+          <div className={styles.main}>
+            {this.props.children}
+          </div>
+  
+          <footer className={styles.footer}></footer>
         </div>
-
-        <footer className={styles.footer}></footer>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 };
 
 export default Main;
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,57 +5,60 @@ import styles from "../styles/Home.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import data from "faker";
 import Main from "../components/Main";
+import React from "react";
 
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>our bissines page</title>
-      </Head>
+export default class Home extends React.Component {
+  render() {
+    return (
+      <>
+        <Head>
+          <title>our bissines page</title>
+        </Head>
 
-      <Main>
-        <div className={styles.homeContainer}>
-          <div className={styles.homePage}>
-            <div className={styles.categories}>
-              <div className={styles.categoriesItem}>
-                <div className={styles.categoriesItemContent}>
-                  <div className={styles.img}>
-                    <img src={data.image.avatar()} />
+        <Main>
+          <div className={styles.homeContainer}>
+            <div className={styles.homePage}>
+              <div className={styles.categories}>
+                <div className={styles.categoriesItem}>
+                  <div className={styles.categoriesItemContent}>
+                    <div className={styles.img}>
+                      <img src={data.image.avatar()} />
+                    </div>
+                    <p className={styles.content}>{data.name.title()}</p>
                   </div>
-                  <p className={styles.content}>{data.name.title()}</p>
                 </div>
-              </div>
 
-              <div className={styles.categoriesItem}>
-                <div className={styles.categoriesItemContent}>
-                  <div className={styles.img}>
-                    <img src={data.image.avatar()} />
+                <div className={styles.categoriesItem}>
+                  <div className={styles.categoriesItemContent}>
+                    <div className={styles.img}>
+                      <img src={data.image.avatar()} />
+                    </div>
+                    <div className={styles.content}>{data.name.title()}</div>
                   </div>
-                  <div className={styles.content}>{data.name.title()}</div>
                 </div>
-              </div>
 
-              <div className={styles.categoriesItem}>
-                <div className={styles.categoriesItemContent}>
-                  <div className={styles.img}>
-                    <img src={data.image.avatar()} />
+                <div className={styles.categoriesItem}>
+                  <div className={styles.categoriesItemContent}>
+                    <div className={styles.img}>
+                      <img src={data.image.avatar()} />
+                    </div>
+                    <div className={styles.content}>{data.name.title()}</div>
                   </div>
-                  <div className={styles.content}>{data.name.title()}</div>
                 </div>
-              </div>
 
-              <div className={styles.categoriesItem}>
-                <div className={styles.categoriesItemContent}>
-                  <div className={styles.img}>
-                    <img src={data.image.avatar()} />
+                <div className={styles.categoriesItem}>
+                  <div className={styles.categoriesItemContent}>
+                    <div className={styles.img}>
+                      <img src={data.image.avatar()} />
+                    </div>
+                    <div className={styles.content}>{data.name.title()}</div>
                   </div>
-                  <div className={styles.content}>{data.name.title()}</div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </Main>
-    </>
-  );
+        </Main>
+      </>
+    );
+  }
 }
